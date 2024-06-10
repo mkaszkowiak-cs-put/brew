940a8d4daaaeae6a49bbcb0d30b80d75f3bdd60d
Adam Jałocha <energywraith@gmail.com>
2024-06-10 23:11:33 +0200

    ZylaCard button is now black as on mockups

--- app/src/main/java/pl/put/brew/CoffeeRatingSuccessScreen.kt
+++ app/src/main/java/pl/put/brew/CoffeeRatingSuccessScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -138,7 +140,11 @@ fun ZylaCard() {
                 Text("Nie bądź źyla!")
                 Text("Podziel się kawą z innymi!", fontWeight = FontWeight.Bold)
                 Spacer(Modifier.height(24.dp))
-                Button(onClick = { /*TODO*/ }) {
+                Button(
+                    onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black
+                    )
+                ) {
                     Text("Z chęcią :-)")
                 }
             }

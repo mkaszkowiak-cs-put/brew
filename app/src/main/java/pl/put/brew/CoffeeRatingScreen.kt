f83f566c4042aa461fbbb725888461a0d7c8ebdd
Adam Jałocha <energywraith@gmail.com>
2024-06-11 10:32:47 +0200

    Add validation to CoffeeRatingScreen

--- app/src/main/java/pl/put/brew/CoffeeRatingScreen.kt
+++ app/src/main/java/pl/put/brew/CoffeeRatingScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
@@ -25,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
@@ -50,7 +53,9 @@ fun CoffeeRatingScreen(
     userModel: UserModel,
     addLocalReviewToCoffee: (Int, Review) -> Unit
 ) {
+    // TODO: Add empty validation
     val reviewModel = remember { ReviewModel(coffeeId = coffee.id, user = userModel.name) }
+    var showError by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
 
     fun addReview() {
@@ -112,8 +117,29 @@ fun CoffeeRatingScreen(
             label = { Text("Treść opinii") },
             minLines = 3,
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    if (reviewModel.review.isBlank()) {
+                        showError = true
+                    } else {
+                        navController.navigate("coffee-list")
+                    }
+                }
+            ),
+            isError = showError,
         )
+        if (showError) {
+            Text(
+                text = "Daj znać co sądzisz o tej kawie!",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
         Spacer(modifier = Modifier.height(32.dp))
         Text("Poziom kawowej euforii")
         Slider(
@@ -139,7 +165,13 @@ fun CoffeeRatingScreen(
             )
         }
         Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { addReview() }) {
+        Button(onClick = {
+            if (reviewModel.review.isBlank()) {
+                showError = true
+            } else {
+                addReview()
+            }
+        }) {
             Text("Ocen kawke")
         }
     }
